847aca012486059ea6d594d0effeb9830b2a8443
sangminlee98 <tkdals0975@naver.com>
2024-07-12 00:53:52 +0900

    [WRFE-6](chore): eslint-rule 설정 (consistent-type-imports 룰 추가)

---
+++ apps/front/wraffle-webview/.eslintrc.js
@@ -0,0 +1,9 @@
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+  root: true,
+  extends: ['@wraffle/eslint-config/next.js'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: true,
+  },
+};
