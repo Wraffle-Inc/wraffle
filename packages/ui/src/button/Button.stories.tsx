6da0b416fd5d420c39c6c97e4ee96e99d8f0e6ea
ww8007 <wshmin1234@gmail.com>
2024-07-12 21:50:19 +0900

    [WRDS-8](chore): storybook 초기 setting

---
+++ packages/ui/src/button/Button.stories.tsx
@@ -0,0 +1,17 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { Button } from "./Button";
+
+const meta: Meta<typeof Button> = {
+	component: Button
+};
+
+export default meta;
+type Story = StoryObj<typeof Button>;
+
+export const Primary: Story = {
+	args: {
+		primary: true,
+		label: "Button"
+	}
+};
