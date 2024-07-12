cce31afa2309cba0048fb2ffb26e4b7d01a0f899
ww8007 <wshmin1234@gmail.com>
2024-07-12 22:54:55 +0900

    [WRDS-8](chore): shadcn setting 추가 및 tailwind setting 추가

---
+++ packages/ui/tailwind.config.ts
@@ -0,0 +1,9 @@
+import type { Config } from "tailwindcss";
+import sharedConfig from "@wraffle/tailwind-config";
+
+const config: Pick<Config, "content" | "presets"> = {
+	content: ["./app/**/*.{ts,tsx}", "./src/**/*.{ts,tsx}"],
+	presets: [sharedConfig]
+};
+
+export default config;
