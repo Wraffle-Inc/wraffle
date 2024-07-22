740e44375bfd058dad4ce4d915513690dbc0c056
HyunTaek Oh <oht222@naver.com>
2024-07-22 17:21:20 +0900

    [WRBE-1](fix): ThrottlerGuard 인터페이스 async 변경에 따른 구현체 리턴타입 Promise 처리

--- apps/back/apps/infrastructure/throttler/throttler.guard.ts
+++ apps/back/apps/infrastructure/throttler/throttler.guard.ts
@@ -1,16 +1,18 @@
-import { ExecutionContext, Injectable } from '@nestjs/common';
-import { ThrottlerGuard as BaseThrottlerGuard } from '@nestjs/throttler';
-import { TooManyRequestsException } from 'apps/infrastructure/error';
+import { ExecutionContext, Injectable } from "@nestjs/common";
+import { ThrottlerGuard as BaseThrottlerGuard } from "@nestjs/throttler";
+import { TooManyRequestsException } from "apps/infrastructure/error";
 
 @Injectable()
 export class ThrottlerGuard extends BaseThrottlerGuard {
-  protected getTracker(req: Record<string, any>): string {
+  protected async getTracker(req: Record<string, any>): Promise<string> {
     const ip = req.ips.length ? req.ips[0] : req.ip;
     const url = req.url;
     return `${ip}-${url}`;
   }
 
-  protected throwThrottlingException(context: ExecutionContext): void {
+  protected async throwThrottlingException(
+    context: ExecutionContext,
+  ): Promise<void> {
     throw new TooManyRequestsException();
   }
 }
