450c473d9916503941f836afe4700e8a573bada7
HyunTaek Oh <oht222@naver.com>
2024-07-31 17:44:15 +0900

    [WRBE-12](feat): 카드 엔티티 추가

---
+++ apps/back/apps/domain/card/card.entity.ts
@@ -0,0 +1,30 @@
+import { DefaultEntity } from "apps/domain/common/default.entity";
+import { Column, Entity, ManyToOne } from "typeorm";
+import { User } from "apps/domain/user/user.entity";
+
+@Entity()
+export class Card extends DefaultEntity {
+  @Column({ comment: "카드 이름" })
+  cardName: string;
+
+  @Column({ comment: "카드사 코드" })
+  cardCode: string;
+
+  @Column({ comment: "카드 번호" })
+  cardNumber: string;
+
+  @Column({ comment: "카드 만료일" })
+  cardExpirationDate: string;
+
+  @Column({ comment: "카드 빌링키" })
+  cardBillingKey: string;
+
+  @Column({ comment: "주사용 카드 여부" })
+  isDefault: boolean;
+
+  @Column({ comment: "사용자 ID" })
+  userId: number;
+
+  @ManyToOne(() => User, (user) => user.cards)
+  user: User;
+}
