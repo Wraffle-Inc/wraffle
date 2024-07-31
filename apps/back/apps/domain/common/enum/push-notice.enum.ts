4992f8804c6f46a17ae46e5dd8e48bce6efa3326
HyunTaek Oh <oht222@naver.com>
2024-07-31 14:58:35 +0900

    [WRBE-12](feat): 푸시알림 엔티티 추가

---
+++ apps/back/apps/domain/common/enum/push-notice.enum.ts
@@ -0,0 +1,6 @@
+export enum PushNoticeType {
+  EVENT_EXPIRE = "EVENT_EXPIRE", // 래플 마감 알림
+  EVENT_DONE = "EVENT_DONE", // 래플 추첨 완료 알림
+  AUTH_ALERT = "AUTH_ALERT", // 인증 관련 알림
+  MARKETING = "MARKETING", // 마케팅 알림
+}
