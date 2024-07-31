1b19f3324863c9a5790068d849ee5325c632bcbe
ajeong <jenny2360@tukorea.ac.kr>
2024-07-31 19:15:24 +0900

    [WRFE-](design): Button component 생성

--- packages/ui/src/ui/button/Button.tsx
+++ packages/ui/src/ui/button/Button.tsx
@@ -5,7 +5,7 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/shared/utils";
 
 const buttonVariants = cva(
-  "w-[335px] px-6 py-3 gap-2.5 inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:cursor-not-allowed disabled:bg-[#F4F4F5] disabled:text-gray-800 disabled:border-none",
+  "w-full px-6 py-3 gap-2.5 inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:cursor-not-allowed disabled:bg-[#F4F4F5] disabled:text-gray-800 disabled:border-none",
   {
     variants: {
       variant: {
