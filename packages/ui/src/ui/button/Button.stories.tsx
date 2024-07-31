6cd1cba7319fc565eb5ddc9b4dca65f313b7e0df
ajeong <jenny2360@tukorea.ac.kr>
2024-07-31 22:47:19 +0900

    [WRDS-15](refactor): disabled 버튼 대신 속성으로 변경

--- packages/ui/src/ui/button/Button.stories.tsx
+++ packages/ui/src/ui/button/Button.stories.tsx
@@ -1,39 +1,32 @@
-import type { Meta, StoryObj } from "@storybook/react";
+import type { Meta, StoryFn } from "@storybook/react";
 
 import { Button } from "./Button";
 
 const meta: Meta<typeof Button> = {
   component: Button,
+  argTypes: {
+    variant: {
+      options: ["default", "gray", "stroke"],
+      control: { type: "select" },
+    },
+    disabled: {
+      control: { type: "boolean" },
+    },
+    children: {
+      control: { type: "text" },
+    },
+  },
 };
 
 export default meta;
-type Story = StoryObj<typeof Button>;
 
-export const Primary: Story = {
-  args: {
-    children: "기본 버튼",
-    variant: "default",
-  },
-};
+const Template: StoryFn = (args) => <Button {...args} />;
 
-export const Gray: Story = {
-  args: {
-    children: "회색 버튼",
-    variant: "gray",
-  },
-};
+export const Primary = Template.bind({});
+Primary.args = { children: "기본 버튼", variant: "default" };
 
-export const Stroke: Story = {
-  args: {
-    children: "테두리 버튼",
-    variant: "stroke",
-  },
-};
+export const Gray = Template.bind({});
+Gray.args = { children: "회색 버튼", variant: "gray" };
 
-export const Disabled: Story = {
-  args: {
-    children: "비활성화 버튼",
-    variant: "disabled",
-    disabled: true,
-  },
-};
+export const Stroke = Template.bind({});
+Stroke.args = { children: "테두리 버튼", variant: "stroke" };
