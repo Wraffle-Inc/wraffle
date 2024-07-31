6586404237e96f7ab9feee5b2ceee6a0874b0c7d
ajeong <jenny2360@tukorea.ac.kr>
2024-07-31 19:15:24 +0900

    [WRDS-15](design): Button component 생성

--- packages/ui/src/ui/button/Button.stories.tsx
+++ packages/ui/src/ui/button/Button.stories.tsx
@@ -3,14 +3,37 @@ import type { Meta, StoryObj } from "@storybook/react";
 import { Button } from "./Button";
 
 const meta: Meta<typeof Button> = {
-	component: Button
+  component: Button,
 };
 
 export default meta;
 type Story = StoryObj<typeof Button>;
 
 export const Primary: Story = {
-	args: {
-		children: "안녕하세요 임시 버튼 입니다"
-	}
+  args: {
+    children: "기본 버튼",
+    variant: "default",
+  },
+};
+
+export const Gray: Story = {
+  args: {
+    children: "회색 버튼",
+    variant: "gray",
+  },
+};
+
+export const Stroke: Story = {
+  args: {
+    children: "테두리 버튼",
+    variant: "stroke",
+  },
+};
+
+export const Disabled: Story = {
+  args: {
+    children: "비활성화 버튼",
+    variant: "disabled",
+    disabled: true,
+  },
 };
