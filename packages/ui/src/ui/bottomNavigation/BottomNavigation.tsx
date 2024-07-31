3b7aef5776e31d49b51c7142afdfafd6c9e08835
gyeong3un2 <gyeong3un2@gmail.com>
2024-07-31 22:28:53 +0900

    [WRDS-12](refactor): 활성화된 아이콘 색상 변경 관련 코드 리팩토링

--- packages/ui/src/ui/bottomNavigation/BottomNavigation.tsx
+++ packages/ui/src/ui/bottomNavigation/BottomNavigation.tsx
@@ -1,52 +1,39 @@
 import { useState } from 'react';
 
-import {
-  HomeIcon,
-  MenuIcon,
-  SearchIcon,
-  GiftIcon,
-  UserIcon,
-} from '@/shared/icons';
-
 import IconWithLabel from '../iconWithLabel/IconWithLabel';
+import SVGIcon from '@/shared/SVGIcon';
 
 const menuItems = [
-  { Icon: MenuIcon, label: '카테고리' },
-  { Icon: SearchIcon, label: '검색' },
-  { Icon: HomeIcon, label: '홈' },
-  { Icon: GiftIcon, label: '래플' },
-  { Icon: UserIcon, label: '내정보' },
+  { id: 'menu', label: '카테고리' },
+  { id: 'search', label: '검색' },
+  { id: 'home', label: '홈' },
+  { id: 'gift', label: '래플' },
+  { id: 'user', label: '내정보' },
 ];
 
 export default function BottomNavigation() {
   const [selectedIcon, setSelectedIcon] = useState<string>('홈');
 
-  const handleIconClick = (label: string) => {
+  const handleClickIcon = (label: string) => {
     setSelectedIcon(label);
   };
 
-  const isSelectedColor = (label: string) => {
-    const text = selectedIcon === label ? 'text-black' : 'text-[#71717A]';
-    const icon = selectedIcon === label ? '#000' : '#71717A';
-
-    return { text, icon };
+  const isSelectedIcon = (label: string) => {
+    return selectedIcon === label;
   };
 
   return (
     <div className="fixed bottom-0 left-0 z-50 w-full border-t bg-[#F9FAFB]">
       <nav className="flex justify-around py-3">
-        {menuItems.map(({ Icon, label }) => (
+        {menuItems.map(({ id, label }) => (
           <IconWithLabel
             key={label}
-            Icon={
-              <Icon
-                className="w-5 h-5"
-                storkeColor={isSelectedColor(label).icon}
-              />
-            }
             label={label}
-            strokeColor={isSelectedColor(label).text}
-            onClick={() => handleIconClick(label)}
+            Icon={<SVGIcon id={id} width={20} height={20} />}
+            strokeColor={
+              isSelectedIcon(label) ? 'text-black' : 'text-[#71717A]'
+            }
+            onClick={() => handleClickIcon(label)}
           />
         ))}
       </nav>
