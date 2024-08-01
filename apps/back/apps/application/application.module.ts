792e21d3db202b8eb5a3f8dbb487b7f8e506d406
hangyeol <snake011128@naver.com>
2024-08-01 18:17:25 +0900

    [WRBE-13](feat): 이벤트 Service, Controller 초기 생성

--- apps/back/apps/application/application.module.ts
+++ apps/back/apps/application/application.module.ts
@@ -1,10 +1,11 @@
 import { Module } from "@nestjs/common";
 
 import { AuthModule } from "./auth/auth.module";
+import {EventModule} from "apps/application/event/event.module";
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, EventModule],
   providers: [],
-  exports: [AuthModule],
+  exports: [AuthModule, EventModule],
 })
 export class ApplicationModule {}
