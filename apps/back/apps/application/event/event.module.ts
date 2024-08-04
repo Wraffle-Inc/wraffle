00bd70720325c033531b1f754aa0b39be226d31e
hangyeol <snake011128@naver.com>
2024-08-04 09:18:16 +0900

    [WRBE-13](fix): 모듈 imports 제거

--- apps/back/apps/application/event/event.module.ts
+++ apps/back/apps/application/event/event.module.ts
@@ -1,11 +1,8 @@
-import { Module } from '@nestjs/common';
-import {TypeOrmModule} from "@nestjs/typeorm";
-import {EventService} from "apps/application/event/service/event.service";
-import {Event} from "apps/domain/event/event.entity";
+import { Module } from "@nestjs/common";
+import { EventService } from "apps/application/event/service/event.service";
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Event])],
-    providers: [EventService],
-    exports: [EventService],
+  providers: [EventService],
+  exports: [EventService],
 })
 export class EventModule {}
