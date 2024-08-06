008b3955611fd7b9e720bde754e81ca6d7eb0918
hangyeol <snake011128@naver.com>
2024-08-06 22:56:04 +0900

    [WRBE-13](feat): 이벤트 조회 기능 구현

--- apps/back/apps/front-api/src/event/event.controller.ts
+++ apps/back/apps/front-api/src/event/event.controller.ts
@@ -1,8 +1,48 @@
-import {Controller} from "@nestjs/common";
-import {EventService} from "apps/application/event/service/event.service";
+import { Body, Controller, Get, Param, Post } from "@nestjs/common";
+import { EventService } from "apps/application/event/service/event.service";
+import { CreateEventDto } from "apps/application/event/dto/request/create-event.dto";
+import {
+  IResponse,
+  ResponseDto,
+} from "apps/application/common/response/response";
+import { CreateEventResultDto } from "apps/application/event/dto/response/create-event-result.dto";
+import { ApiOkResponse, ApiOperation } from "@nestjs/swagger";
+import { Public } from "apps/application/common/auth/public.decorator";
+import { GetEventDto } from "apps/application/event/dto/response/get-event.dto";
 
 @Controller("events")
 export class EventController {
-    constructor(private readonly eventService: EventService) {
-    }
+  constructor(private readonly eventService: EventService) {}
+
+  // TODO: @CurrentUser()로 사용자 정보 가져와서 createUserId에 넣어주기, @Public() 제거
+  @ApiOperation({
+    summary: "이벤트 생성",
+    operationId: "createEvent",
+    tags: ["event"],
+  })
+  @ApiOkResponse({
+    type: ResponseDto(CreateEventResultDto, "CreateEventResult"),
+  })
+  @Post()
+  @Public()
+  async createEvent(
+    @Body() dto: CreateEventDto,
+    // @CurrentUser() user: any,
+  ): Promise<IResponse<CreateEventResultDto>> {
+    return this.eventService.createEvent(dto);
+  }
+
+  @ApiOperation({
+    summary: "이벤트 조회",
+    operationId: "getEventById",
+    tags: ["event"],
+  })
+  @ApiOkResponse({
+    type: ResponseDto(GetEventDto, "GetEvent"),
+  })
+  @Get(":id")
+  @Public()
+  async getEventById(@Param("id") id: number): Promise<IResponse<GetEventDto>> {
+    return this.eventService.getEventById(id);
+  }
 }
