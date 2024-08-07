33f1577421ffd0dfca66a0e14b8dde70760c28c0
ajeong <jenny2360@tukorea.ac.kr>
2024-08-07 16:47:03 +0900

    [WRDS-18](feat): Round Button 컴포넌트 추가

---
+++ packages/ui/src/ui/roundButton/RoundButton.stories.tsx
@@ -0,0 +1,27 @@
+import type { Meta, StoryFn } from "@storybook/react";
+import { RoundButton } from "./RoundButton";
+
+const meta: Meta<typeof RoundButton> = {
+  title: "Components/RoundButton",
+  component: RoundButton,
+  argTypes: {
+    selected: {
+      control: { type: "boolean" },
+    },
+    children: {
+      control: { type: "text" },
+    },
+  },
+};
+
+export default meta;
+
+const Template: StoryFn<typeof RoundButton> = (args) => (
+  <RoundButton {...args} />
+);
+
+export const Button = Template.bind({});
+Button.args = {
+  children: "Category",
+  selected: false,
+};
