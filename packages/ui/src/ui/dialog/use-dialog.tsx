2239a2d418bc019506082aae817ab1fc746c5826
ww8007 <wshmin1234@gmail.com>
2024-08-11 21:53:48 +0900

    [WRDS-17](feat): customHook으로 Dialog를 사용할 수 있도록 추가

---
+++ packages/ui/src/ui/dialog/use-dialog.tsx
@@ -0,0 +1,103 @@
+"use client";
+
+import * as React from "react";
+import {
+	Dialog,
+	DialogContent,
+	DialogClose,
+	DialogHeader,
+	DialogFooter,
+	DialogTitle,
+	DialogDescription
+} from "./Dialog";
+import { Button } from "../button/Button";
+import { cn } from "@/shared/utils";
+import { DialogTrigger } from "@radix-ui/react-dialog";
+
+type DialogProps = {
+	title: React.ReactNode;
+	description: React.ReactNode;
+	footer?: [string] | [string, string];
+	withCloseButton?: boolean;
+	justify?: "center" | "left";
+};
+
+type DialogState = {
+	props: DialogProps;
+	isOpen: boolean;
+};
+
+const INITIAL_STATE: DialogState = {
+	props: {
+		title: "",
+		description: "",
+		footer: undefined,
+		withCloseButton: false,
+		justify: "left"
+	},
+	isOpen: false
+};
+
+const DialogContext = React.createContext<{
+	state: DialogState;
+	openDialog: (props: DialogProps) => void;
+	closeDialog: () => void;
+}>({
+	state: INITIAL_STATE,
+	openDialog: () => {},
+	closeDialog: () => {}
+});
+
+interface DialogProviderProps {
+	children: React.ReactNode;
+}
+
+export const DialogProvider = ({ children }: DialogProviderProps) => {
+	const [state, setState] = React.useState<DialogState>(INITIAL_STATE);
+
+	const openDialog = (props: DialogProps) => {
+		setState({ props, isOpen: true });
+	};
+
+	const closeDialog = () => {
+		setState(INITIAL_STATE);
+	};
+
+	return (
+		<DialogContext.Provider value={{ state, openDialog, closeDialog }}>
+			<Dialog
+				open={state.isOpen}
+				onOpenChange={(isOpen) => !isOpen && closeDialog()}
+			>
+				{!state.isOpen && <DialogTrigger>{children}</DialogTrigger>}
+				<DialogContent withCloseButton={state.props.withCloseButton}>
+					<DialogHeader
+						className={cn(
+							state.props.justify === "center"
+								? "justify-center"
+								: "justify-start"
+						)}
+					>
+						<DialogTitle>{state.props.title}</DialogTitle>
+						<DialogDescription>{state.props.description}</DialogDescription>
+					</DialogHeader>
+					<DialogFooter className='flex gap-1'>
+						{state.props.footer?.map((text, index) => (
+							<DialogClose asChild key={index}>
+								<Button>{text}</Button>
+							</DialogClose>
+						))}
+					</DialogFooter>
+				</DialogContent>
+			</Dialog>
+		</DialogContext.Provider>
+	);
+};
+
+export const useDialog = () => {
+	const context = React.useContext(DialogContext);
+	if (!context) {
+		throw new Error("useDialog는 DialogProvider 안에서 사용해야 합니다.");
+	}
+	return context;
+};
