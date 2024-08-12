01081ec9696aa59c87431243cb515ed5a03553bc
희은 <datakubit2@gmail.com>
2024-08-12 19:41:42 +0900

    [WRBE-25](feat): 유저 부분, PG사 연동 제외 카드 등록 기능 구현

--- apps/back/apps/application/card/service/card.service.ts
+++ apps/back/apps/application/card/service/card.service.ts
@@ -6,7 +6,10 @@ import {
   IResponse,
 } from "apps/application/common/response/response";
 import { Card } from "apps/domain/card/card.entity";
+import { CreateCardDto } from "apps/application/card/dto/request/create-card.dto";
+import { CreateCardResultDto } from "apps/application/card/dto/response/create-card-result.dto";
 import { Transactional } from "typeorm-transactional";
+import { plainToInstance } from "class-transformer";
 
 @Injectable()
 export class CardService {
@@ -14,4 +17,35 @@ export class CardService {
     @InjectRepository(Card)
     private readonly cardRepository: Repository<Card>
   ) {}
+
+  @Transactional()
+  async createCard(
+    dto: CreateCardDto
+  ): Promise<IResponse<CreateCardResultDto>> {
+    // 카드 등록
+    // TODO: SignUp API 추가되면, @CurrentUser()로 사용자 정보 가져오기
+    const card = this.cardRepository.create({
+      ...dto,
+      userId: 1,
+    });
+
+    await this.cardRepository.save(card);
+
+    // 카드 등록 결과 DTO 생성
+    const createCardResultDto = plainToInstance(CreateCardResultDto, {
+      id: card.id,
+      cardName: card.cardName,
+      cardCode: card.cardCode,
+      cardNumber: card.cardNumber,
+      cardExpirationDate: card.cardExpirationDate,
+      billingKey: card.billingKey,
+      isDefault: card.isDefault,
+    });
+
+    return new CustomResponse<CreateCardResultDto>(
+      200,
+      "C001",
+      createCardResultDto
+    );
+  }
 }
