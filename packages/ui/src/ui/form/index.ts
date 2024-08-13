c4b6f0025b597d8e88aeaad69be3fc7112e2deb1
sangminlee98 <tkdals0975@naver.com>
2024-08-14 04:54:42 +0900

    [WRDS-19](fix): PrimitiveForm과 react-hook-form 의존성 제거

--- packages/ui/src/ui/form/index.ts
+++ packages/ui/src/ui/form/index.ts
@@ -2,4 +2,3 @@ export * from "./input/Input";
 export * from "./inputField/InputField";
 export * from "./errorMessage/ErrorMessage";
 export * from "./label/Label";
-export * from "./PrimitiveForm";
