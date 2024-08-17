9c9624df98382019e96e7ce599e2ea0305092602
hangyeol <snake011128@naver.com>
2024-08-17 23:01:16 +0900

    [WRBE-13](fix): 모듈에서 controller 명시 삭제

--- apps/back/apps/application/event/event.module.ts
+++ apps/back/apps/application/event/event.module.ts
@@ -1,10 +1,8 @@
 import { Module } from "@nestjs/common";
 import { EventService } from "apps/application/event/service/event.service";
-import { EventController } from "apps/front-api/src/event/event.controller";
 
 @Module({
   providers: [EventService],
   exports: [EventService],
-  controllers: [EventController],
 })
 export class EventModule {}
