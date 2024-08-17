8ee27383551c9bff18da0719116964257888e7b0
hangyeol <snake011128@naver.com>
2024-08-17 23:16:48 +0900

    [WRBE-13](feat): interceptor 부분에 TODO 작성
    TODO 확인 필요

--- apps/back/apps/application/event/interceptor/event-view-count.interceptor.ts
+++ apps/back/apps/application/event/interceptor/event-view-count.interceptor.ts
@@ -11,6 +11,7 @@ import { Observable, tap } from "rxjs";
 export class EventViewCountInterceptor implements NestInterceptor {
   constructor(private readonly eventService: EventService) {}
 
+  // TODO: 추후 Redis에 키로 저장하여 스케쥴러로 한 번에 업데이트 하도록 구현
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     const request = context.switchToHttp().getRequest();
     const eventId = request.params.id;
