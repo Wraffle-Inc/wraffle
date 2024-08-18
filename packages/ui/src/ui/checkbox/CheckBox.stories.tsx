f0e73d2e3b2bc1ce9be35d95ee41f2650f2d788e
hwang <hgh1057@gmail.com>
2024-08-18 16:13:05 +0900

    CheckBox mdx 문서 추가 및 리뷰 수정

--- packages/ui/src/ui/checkbox/CheckBox.stories.tsx
+++ packages/ui/src/ui/checkbox/CheckBox.stories.tsx
@@ -4,7 +4,6 @@ import CheckBox from './CheckBox';
 const meta = {
   component: CheckBox,
   title: 'Components/CheckBox',
-  tags: ['autodocs'],
 } satisfies Meta<typeof CheckBox>;
 
 export default meta;
@@ -13,28 +12,96 @@ type Story = StoryObj<typeof meta>;
 
 export const CheckBoxDefault: Story = {
   name: 'CheckBox',
-  args: { label: '' },
+  args: { id: 'default', label: 'default', onCheckedChange: () => {} },
 };
 
 export const CheckBoxDisabled: Story = {
   name: 'CheckBox-Disabled',
-  args: { label: '', disabled: true },
+  args: {
+    id: 'disabled',
+    label: 'disabled',
+    disabled: true,
+    onCheckedChange: () => {},
+  },
 };
 
 export const CheckBoxChecked: Story = {
   name: 'CheckBox-Checked',
-  args: { label: '', checked: true },
+  args: {
+    id: 'checked',
+    label: 'checked',
+    checked: true,
+    onCheckedChange: () => {},
+  },
 };
 
 export const CheckBoxCheckedDisabled: Story = {
   name: 'CheckBox-Checked-Disabled',
-  args: { label: '', checked: true, disabled: true },
+  args: {
+    id: 'checkedDisabled',
+    label: 'checkedDisabled',
+    checked: true,
+    disabled: true,
+    onCheckedChange: () => {},
+  },
 };
 
 export const CheckBoxWithLabel: Story = {
   name: 'CheckBox-Label',
   args: {
+    id: 'asdfadsfsadfsadfasdfsadfsdfadsfdf',
     label: 'asdfadsfsadfsadfasdfsadfsdfadsfdf',
-    labelClassName: "text-red-500",
+    className: 'text-red-500',
+    onCheckedChange: () => {},
   },
+};
+
+export const CheckBoxGroup: Story = {
+  name: 'CheckBox-Group',
+  args: { id: '', onCheckedChange: () => {} },
+  render: () => (
+    <div className='flex flex-col gap-2'>
+      <CheckBox id='group1' label='Option 1' onCheckedChange={() => {}} />
+      <CheckBox id='group2' label='Option 2' onCheckedChange={() => {}} />
+      <CheckBox id='group3' label='Option 3' onCheckedChange={() => {}} />
+      <CheckBox id='group4' label='Option 4' onCheckedChange={() => {}} />
+    </div>
+  ),
+};
+
+export const CheckBoxPreview: Story = {
+  name: 'CheckBox-Preview',
+  args: { id: '', onCheckedChange: () => {} },
+  render: () => (
+    <div className='flex flex-col gap-2'>
+      <CheckBox id='group1' label='' onCheckedChange={() => {}} />
+      <CheckBox
+        id='group2'
+        label=''
+        disabled={true}
+        onCheckedChange={() => {}}
+        className='cursor-not-allowed'
+      />
+      <CheckBox
+        id='group3'
+        label=''
+        checked={true}
+        onCheckedChange={() => {}}
+      />
+      <CheckBox
+        id='group4'
+        label=''
+        checked={true}
+        disabled={true}
+        onCheckedChange={() => {}}
+        className='cursor-not-allowed'
+      />
+      <CheckBox
+        id='group5'
+        label='label test'
+        onCheckedChange={() => {}}
+        className='text-red-1 text-xl'
+      />
+    </div>
+  ),
 };
