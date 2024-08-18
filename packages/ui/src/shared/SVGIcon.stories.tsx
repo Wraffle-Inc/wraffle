406a72f22dda53750bcdd0743cb1b9f4ed6abc94
gyeong3un2 <gyeong3un2@gmail.com>
2024-08-18 20:51:01 +0900

    [WRDS-12](feat): 북마크 svg 수정 및 docs 추가

--- packages/ui/src/shared/SVGIcon.stories.tsx
+++ packages/ui/src/shared/SVGIcon.stories.tsx
@@ -4,18 +4,96 @@ import SVGIcon from './SVGIcon';
 const meta = {
   component: SVGIcon,
   title: 'shared/SVGIcon',
+  argTypes: {
+    id: {
+      description: '아이콘의 아이디',
+      control: {
+        type: 'select',
+        options: [
+          'arrow-bottom',
+          'arrow-right',
+          'arrow-left',
+          'menu',
+          'search',
+          'home',
+          'gift',
+          'user-circle',
+          'cart',
+          'shopping-box',
+          'setting',
+          'plus',
+          'bell',
+          'bookmark',
+          'bookmark-fill',
+          'heart',
+          'heart-fill',
+          'user',
+          'credit-card',
+          'upload',
+          'calendar',
+          'write',
+        ],
+      },
+    },
+    width: {
+      description: '아이콘의 너비',
+      table: {
+        type: { summary: 'number' },
+      },
+      control: {
+        type: 'number',
+        value: {
+          defaultValue: 20,
+        },
+      },
+    },
+    height: {
+      description: '아이콘의 높이',
+      table: {
+        type: { summary: 'number' },
+      },
+      control: {
+        type: 'number',
+        value: {
+          defaultValue: 20,
+        },
+      },
+    },
+    badgeCount: {
+      description: '아이콘 오른쪽 상단에 표시할 숫자',
+      table: {
+        type: { summary: 'number' },
+      },
+      control: {
+        value: {
+          defaultValue: null,
+        },
+      },
+    },
+    showBadge: {
+      description: '아이콘 오른쪽 상단에 뱃지를 표시할지 여부',
+      table: {
+        type: { summary: 'boolean' },
+      },
+    },
+  },
 } satisfies Meta<typeof SVGIcon>;
 
 export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const SVGIconSearch: Story = {
-  name: 'SVGIcon-Search',
-  args: { id: 'search' },
+export const Default: Story = {
+  name: 'Default',
+  args: { id: 'home' },
 };
 
-export const SVGIconMenu: Story = {
-  name: 'SVGIcon-Menu',
-  args: { id: 'menu' },
+export const SVGIconWithBadge: Story = {
+  name: 'SVGIcon-Badge',
+  args: { id: 'bell', showBadge: true },
+};
+
+export const SVGIconWithBadgeCount: Story = {
+  name: 'SVGIcon-BadgeCount',
+  args: { id: 'cart', showBadge: true, badgeCount: 10 },
 };
--- packages/ui/src/shared/SVGIcon.tsx
+++ packages/ui/src/shared/SVGIcon.tsx
@@ -1,15 +1,59 @@
-export type SvgIconId = 'menu' | 'search' | 'home' | 'gift' | 'user';
+import clsx from 'clsx';
+
+export type SvgIconId =
+  | 'arrow-bottom'
+  | 'arrow-right'
+  | 'arrow-left'
+  | 'menu'
+  | 'search'
+  | 'home'
+  | 'gift'
+  | 'user-circle'
+  | 'cart'
+  | 'shopping-box'
+  | 'setting'
+  | 'plus'
+  | 'bell'
+  | 'bookmark'
+  | 'bookmark-fill'
+  | 'heart'
+  | 'heart-fill'
+  | 'user'
+  | 'credit-card'
+  | 'upload'
+  | 'calendar'
+  | 'write';
 
 interface SvgIconProps {
-  id: string;
+  id: SvgIconId;
   width?: number | string;
   height?: number | string;
+  badgeCount?: number;
+  showBadge?: boolean;
 }
 
-const SVGIcon = ({ id, width = 20, height = 20 }: SvgIconProps) => (
-  <svg fill="none" width={width} height={height}>
-    <use href={`/icon.svg#icon-${id}`} />
-  </svg>
+const SVGIcon = ({
+  id,
+  width = 20,
+  height = 20,
+  badgeCount = 0,
+  showBadge = false,
+}: SvgIconProps) => (
+  <div style={{ position: 'absolute' }}>
+    <svg fill="none" width={width} height={height}>
+      <use href={`/icon.svg#icon-${id}`} />
+    </svg>
+    {showBadge && (
+      <div
+        className={clsx(
+          'absolute bg-red-500 rounded-full flex justify-center items-center text-white text-xs',
+          badgeCount > 0 ? '-top-2 -right-2 w-4 h-4' : 'top-0 right-0 w-2 h-2'
+        )}
+      >
+        {badgeCount > 0 && badgeCount}
+      </div>
+    )}
+  </div>
 );
 
 export default SVGIcon;
