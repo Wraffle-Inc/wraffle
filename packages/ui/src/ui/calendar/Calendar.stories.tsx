d7a450d9aaa8e45d09cfb95f63ff30ec70fc1b2f
hwang <hgh1057@gmail.com>
2024-08-19 07:50:30 +0900

    [WRDS-22](feat): Calendar컴포넌트 구현 진행 중 임시 커밋

---
+++ packages/ui/src/ui/calendar/Calendar.stories.tsx
@@ -0,0 +1,16 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Calendar } from './Calendar';
+
+const meta = {
+  component: Calendar,
+  title: 'Components/Calendar',
+} satisfies Meta<typeof Calendar>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const CalendarDefault: Story = {
+  name: 'Calendar',
+  args: {},
+};
