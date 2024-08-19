ded4d84bfc3cd4266634c282e2484969e77b365e
woohee <aldkgha@naver.com>
2024-08-20 05:20:42 +0900

    [WRFE-16](refactor): 변수명 변경 및 클래스 구성 로직 간소화

--- packages/ui/src/ui/typography/Typography.tsx
+++ packages/ui/src/ui/typography/Typography.tsx
@@ -1,42 +1,70 @@
 import * as React from "react";
 import { Slot } from "@radix-ui/react-slot";
+import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/shared/utils";
 
-const baseStyles = "font-pretendard";
+const typographyVariants = cva("font-pretendard", {
+  variants: {
+    fontSize: {
+      h1: "text-h1 font-bold leading-[1.4]",
+      h2: "text-h2 font-bold leading-[1.4]",
+      h3: "text-h3 font-semibold leading-[1.4]",
+      h4: "text-h4 font-semibold leading-[1.4]",
+      h5: "text-h5 font-semibold leading-[1.4]",
+      h6: "text-h6 font-semibold leading-[1.4]",
+      p1: "text-p1 font-medium leading-[1.4]",
+      p2: "text-p2 font-medium leading-[1.4]",
+      p3: "text-p3 font-medium leading-[1.4]",
+      p4: "text-p4 font-medium leading-[1.4]",
+      sm1: "text-sm1 font-medium leading-[1.4]",
+      sm2: "text-sm2 font-medium leading-[1.4]",
+    },
+    fontWeight: {
+      medium: "font-medium",
+      semibold: "font-semibold",
+      bold: "font-bold",
+    },
+    lineHeight: {
+      tight: "leading-tight",
+    },
+  },
+  defaultVariants: {
+    fontSize: "p1",
+    fontWeight: "medium",
+    lineHeight: "tight",
+  },
+});
 
-const typographyVariants = {
-  h1: "text-h1 font-bold leading-[1.4]",
-  h2: "text-h2 font-bold leading-[1.4]",
-  h3: "text-h3 font-semibold leading-[1.4]",
-  h4: "text-h4 font-semibold leading-[1.4]",
-  h5: "text-h5 font-semibold leading-[1.4]",
-  h6: "text-h6 font-semibold leading-[1.4]",
-  p1: "text-p1 font-medium leading-[1.4]",
-  p2: "text-p2 font-medium leading-[1.4]",
-  p3: "text-p3 font-medium leading-[1.4]",
-  p4: "text-p4 font-medium leading-[1.4]",
-  sm1: "text-sm1 font-medium leading-[1.4]",
-  sm2: "text-sm2 font-medium leading-[1.4]",
-};
-
-export interface TypographyProps extends React.HTMLAttributes<HTMLElement> {
-  variant: keyof typeof typographyVariants;
+export interface TypographyProps
+  extends React.HTMLAttributes<HTMLElement>,
+    VariantProps<typeof typographyVariants> {
   asChild?: boolean;
 }
 
 export const Typography = React.forwardRef<HTMLElement, TypographyProps>(
-  ({ className, variant, asChild = false, ...props }, ref) => {
-    // const Comp = asChild ? Slot : "div";
+  (
+    { className, fontSize, fontWeight, lineHeight, asChild = false, ...props },
+    ref
+  ) => {
     const Comp: React.ElementType = asChild
       ? Slot
-      : ["h1", "h2", "h3", "h4", "h5", "h6"].includes(variant)
-        ? (variant as React.ElementType)
+      : ["h1", "h2", "h3", "h4", "h5", "h6"].includes(fontSize!)
+        ? (fontSize as React.ElementType)
         : "p";
 
-    const classes = cn(baseStyles, typographyVariants[variant], className);
-    return <Comp className={classes} ref={ref} {...props} />;
+    return (
+      <Comp
+        className={cn(
+          typographyVariants({ fontSize, fontWeight, lineHeight }),
+          className
+        )}
+        ref={ref}
+        {...props}
+      />
+    );
   }
 );
+
 Typography.displayName = "Typography";
 
 export { typographyVariants };
