ff9a06263b4e5c92cccccf19dd9a568b33d3ae65
woohee <aldkgha@naver.com>
2024-08-20 05:25:47 +0900

    [WRDS-16](refactor):  Story Template 간소화 및 불필요한 코드 삭제

--- packages/ui/src/ui/typography/Typography.stories.tsx
+++ packages/ui/src/ui/typography/Typography.stories.tsx
@@ -1,72 +1,109 @@
-import type { Meta, StoryObj } from '@storybook/react';
-import { Typography, TypographyProps } from './Typography';
+import type { Meta, StoryFn } from "@storybook/react";
+import { Typography, TypographyProps } from "./Typography";
 
-const meta: Meta<typeof Typography> = {
-  title: 'Foundations/Typography',
+const meta: Meta<TypographyProps> = {
+  title: "Foundations/Typography",
   component: Typography,
   argTypes: {
-    variant: {
+    fontSize: {
       control: {
-        type: 'select',
+        type: "select",
         options: [
-          'heading-1',
-          'heading-2',
-          'heading-3',
-          'heading-4',
-          'heading-5',
-          'heading-6',
-          'paragraph-1',
-          'paragraph-2',
-          'paragraph-3',
-          'paragraph-4',
-          'smalltext-1',
-          'smalltext-2',
+          "h1",
+          "h2",
+          "h3",
+          "h4",
+          "h5",
+          "h6",
+          "p1",
+          "p2",
+          "p3",
+          "p4",
+          "sm1",
+          "sm2",
         ],
       },
     },
+    fontWeight: {
+      control: {
+        type: "select",
+        options: ["medium", "semibold", "bold"],
+      },
+    },
+    lineHeight: {
+      control: {
+        type: "text",
+      },
+      defaultValue: "tight",
+    },
+    children: {
+      control: {
+        type: "text",
+      },
+    },
   },
-  tags: ['autodocs'],
 };
 
 export default meta;
-type Story = StoryObj<typeof Typography>;
 
-const createStory = (
-  variant: TypographyProps['variant'],
-  children: React.ReactNode
-): Story => ({
-  args: {
-    variant,
-    children,
-  },
-});
+const Template: StoryFn<TypographyProps> = (args) => <Typography {...args} />;
 
-// Heading Variants
-export const Heading = () => (
-  <>
-    <Typography variant='h1'>Heading 1 (32px, Bold, 140%)</Typography>
-    <Typography variant='h2'>Heading 2 (24px, Bold, 140%)</Typography>
-    <Typography variant='h3'>Heading 3 (20px, Semi Bold, 140%)</Typography>
-    <Typography variant='h4'>Heading 4 (17px, Semi Bold, 140%)</Typography>
-    <Typography variant='h5'>Heading 5 (16px, Semi Bold, 140%)</Typography>
-    <Typography variant='h6'>Heading 6 (15px, Semi Bold, 140%)</Typography>
-  </>
-);
+export const Heading = Template.bind({});
+Heading.args = {
+  children: (
+    <>
+      <Typography fontSize="h1" fontWeight="bold" lineHeight="tight">
+        Heading 1 (32px, Bold, 140%)
+      </Typography>
+      <Typography fontSize="h2" fontWeight="bold" lineHeight="tight">
+        Heading 2 (24px, Bold, 140%)
+      </Typography>
+      <Typography fontSize="h3" fontWeight="semibold" lineHeight="tight">
+        Heading 3 (20px, Semi Bold, 140%)
+      </Typography>
+      <Typography fontSize="h4" fontWeight="semibold" lineHeight="tight">
+        Heading 4 (17px, Semi Bold, 140%)
+      </Typography>
+      <Typography fontSize="h5" fontWeight="semibold" lineHeight="tight">
+        Heading 5 (16px, Semi Bold, 140%)
+      </Typography>
+      <Typography fontSize="h6" fontWeight="semibold" lineHeight="tight">
+        Heading 6 (15px, Semi Bold, 140%)
+      </Typography>
+    </>
+  ),
+};
 
-// Paragraph Variants
-export const Paragraph = () => (
-  <>
-    <Typography variant='p1'>Paragraph 1</Typography>
-    <Typography variant='p2'>Paragraph 2</Typography>
-    <Typography variant='p3'>Paragraph 3</Typography>
-    <Typography variant='p4'>Paragraph 4</Typography>
-  </>
-);
+export const Paragraph = Template.bind({});
+Paragraph.args = {
+  children: (
+    <>
+      <Typography fontSize="p1" fontWeight="medium" lineHeight="tight">
+        Paragraph 1 (16px, Medium, 140%)
+      </Typography>
+      <Typography fontSize="p2" fontWeight="medium" lineHeight="tight">
+        Paragraph 2 (14px, Medium, 140%)
+      </Typography>
+      <Typography fontSize="p3" fontWeight="medium" lineHeight="tight">
+        Paragraph 3 (14px, Medium, 140%)
+      </Typography>
+      <Typography fontSize="p4" fontWeight="medium" lineHeight="tight">
+        Paragraph 4 (13px, Medium, 140%)
+      </Typography>
+    </>
+  ),
+};
 
-// SmallText Variants
-export const SmallText = () => (
-  <>
-    <Typography variant='sm1'>SmallText 1</Typography>
-    <Typography variant='sm2'>SmallText 2</Typography>
-  </>
-);
+export const SmallText = Template.bind({});
+SmallText.args = {
+  children: (
+    <>
+      <Typography fontSize="sm1" fontWeight="medium" lineHeight="tight">
+        Small Text 1 (12px, Medium, 140%)
+      </Typography>
+      <Typography fontSize="sm2" fontWeight="medium" lineHeight="tight">
+        Small Text 2 (10px, Medium, 140%)
+      </Typography>
+    </>
+  ),
+};
