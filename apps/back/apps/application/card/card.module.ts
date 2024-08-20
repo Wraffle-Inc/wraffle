74bfd99a7d6a1c44766a07fb5f1460eddf5a9743
희은 <datakubit2@gmail.com>
2024-08-21 03:04:39 +0900

    [WRBE-25](fix): CardModule에서 CardController 삭제

--- apps/back/apps/application/card/card.module.ts
+++ apps/back/apps/application/card/card.module.ts
@@ -1,10 +1,8 @@
 import { Module } from "@nestjs/common";
 import { CardService } from "apps/application/card/service/card.service";
-import { CardController } from "apps/front-api/src/card/card.controller";
 
 @Module({
   providers: [CardService],
   exports: [CardService],
-  controllers: [CardController],
 })
 export class CardModule {}
