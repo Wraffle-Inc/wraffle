fa46214cebf0b524eed47bc39f026e40497064d8
hwang <hgh1057@gmail.com>
2024-08-27 20:11:27 +0900

    [WRDS-22](feat): Calendar컴포넌트 추가

--- packages/ui/src/ui/calendar/Calendar.stories.tsx
+++ packages/ui/src/ui/calendar/Calendar.stories.tsx
@@ -1,16 +1,32 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import Calendar from './Calendar';
+import CalendarForm from './Calendar';
+import { useState } from 'react';
 
 const meta = {
-  component: Calendar,
+  component: CalendarForm,
   title: 'Components/Calendar',
-} satisfies Meta<typeof Calendar>;
+} satisfies Meta<typeof CalendarForm>;
 
 export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const CalendarDefault: Story = {
-  name: 'Calendar',
-  args: {},
+const CalendarForRender = (args: any) => {
+  const [selected, setSelected] = useState<Date | undefined>();
+
+  return (
+    <CalendarForm {...args} selected={selected} setSelected={setSelected} />
+  );
+};
+
+export const CalendarFormDefault: Story = {
+  name: 'CalendarForm',
+  args: {
+    formLabel: '어떤 일정을 정할지에 대해 작성하는 라벨입니다.',
+    className: '',
+    dateLabel: '선택한 날짜가 표시되기 전 보여지는 라벨입니다',
+    selected: undefined,
+    setSelected: () => {},
+  },
+  render: (args) => <CalendarForRender {...args} />,
 };
