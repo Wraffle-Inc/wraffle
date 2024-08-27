6bc06d0674c0b00708885dbee64c7d3a3d880f30
ww8007 <wshmin1234@gmail.com>
2024-08-28 02:35:22 +0900

    [WRDS-20](fix): backend repo 이전으로 삭제

--- apps/back/apps/application/event/event.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from "@nestjs/common";
-import { EventService } from "apps/application/event/service/event.service";
-
-@Module({
-  providers: [EventService],
-  exports: [EventService],
-})
-export class EventModule {}
--- apps/back/apps/domain/common/enum/payment.enum.ts
+++
@@ -1,4 +0,0 @@
-export enum PaymentMethod {
-  CARD = "card",
-  FREE = "free", //0원 결제용
-}
