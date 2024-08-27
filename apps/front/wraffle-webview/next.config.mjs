b72db5ca0c1cdf2714d635aa225a05c321f0a039
ww8007 <wshmin1234@gmail.com>
2024-08-28 02:36:20 +0900

    [WRDS-17](chore): 디자인 시스템 alias를 인식 못하는 문제로 상대 경로 기반으로 찾아갈 수 있도록 추가

--- apps/front/wraffle-webview/next.config.mjs
+++ apps/front/wraffle-webview/next.config.mjs
@@ -1,4 +1,23 @@
+import path from 'path';
+import {fileURLToPath} from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: config => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@wds/ui': path.resolve(__dirname, '../../../packages/ui/src/ui'),
+      '@wds/shared': path.resolve(__dirname, '../../../packages/ui/src/shared'),
+      '@wds/widgets': path.resolve(
+        __dirname,
+        '../../../packages/ui/src/widgets',
+      ),
+    };
+    return config;
+  },
+};
 
 export default nextConfig;
