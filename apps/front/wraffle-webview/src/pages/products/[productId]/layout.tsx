43d1970e758eb5fe6d3cee215ff8306468fd6aff
ajeong <jenny2360@tukorea.ac.kr>
2024-08-31 06:26:23 +0900

    [WRFE-10](feat): ProductInfoMenu 컴포넌트 추가

--- apps/front/wraffle-webview/src/pages/products/[productId]/layout.tsx
+++ apps/front/wraffle-webview/src/pages/products/[productId]/layout.tsx
@@ -1,12 +1,26 @@
-import type {ReactNode} from 'react';
+'use client';
 
-type Props = {children: ReactNode};
+import type {ReactNode} from 'react';
+import React from 'react';
+import {ProductInfoMenu} from '@/widgets/product-info';
+
+type Props = {
+  children: ReactNode;
+};
+
 export default function Layout({children}: Props) {
+  const [selectedMenu, setSelectedMenu] = React.useState<string>('상품'); // 선택된 메뉴 상태 관리
+
   return (
     <>
-      <header className="mb-[25px] mt-[25px] h-[56px] bg-slate-700" />
-      <header className="mb-[25px] h-[34px] bg-slate-700" />
-      {children}
+      <header className='mb-[25px] mt-[25px] h-[56px] bg-slate-700' />
+
+      <ProductInfoMenu
+        selectedMenu={selectedMenu}
+        onSelectMenu={setSelectedMenu}
+      />
+
+      <main>{children}</main>
     </>
   );
 }
