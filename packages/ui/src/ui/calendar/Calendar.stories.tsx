ff6b4d13b5f5dddab98a36514442f8f7a5364488
hwang <hgh1057@gmail.com>
2024-09-02 04:14:05 +0900

    [WRDS-22](fix):  CSF3 형식으로 수정 및 스토리 추가

--- packages/ui/src/ui/calendar/Calendar.stories.tsx
+++ packages/ui/src/ui/calendar/Calendar.stories.tsx
@@ -1,24 +1,38 @@
-import type { Meta, StoryFn } from '@storybook/react';
+import type { Meta, StoryObj } from '@storybook/react';
 import CalendarForm from './Calendar';
 import { useState } from 'react';
 
-const meta = {
+const meta: Meta<typeof CalendarForm> = {
   component: CalendarForm,
   title: 'Components/Calendar',
-} satisfies Meta<typeof CalendarForm>;
+};
 
 export default meta;
 
-const Template: StoryFn<typeof CalendarForm> = (args) => {
-  const [selected, setSelected] = useState<Date | undefined>();
-  return (
-    <CalendarForm {...args} selected={selected} setSelected={setSelected} />
-  );
+type Story = StoryObj<typeof meta>;
+
+export const CalendarFormDefault: Story = {
+  name: 'Default',
+  args: {
+    formLabel: '어떤 일정을 정할지에 대해 작성하는 라벨입니다.',
+    className: '',
+    dateLabel: '선택한 날짜가 표시되기 전 보여지는 라벨입니다',
+  },
+  render: (args) => {
+    const [selected, setSelected] = useState<Date | undefined>();
+    return (
+      <CalendarForm {...args} selected={selected} setSelected={setSelected} />
+    );
+  },
 };
 
-export const CalendarFormDefault = Template.bind({});
-CalendarFormDefault.args = {
-  formLabel: '어떤 일정을 정할지에 대해 작성하는 라벨입니다.',
-  className: '',
-  dateLabel: '선택한 날짜가 표시되기 전 보여지는 라벨입니다',
+export const CalendarFormSelected: Story = {
+  name: 'Selected',
+  args: { ...CalendarFormDefault.args, className: 'text-blue-300' },
+  render: (args) => {
+    const [selected, setSelected] = useState<Date | undefined>(new Date());
+    return (
+      <CalendarForm {...args} selected={selected} setSelected={setSelected} />
+    );
+  },
 };
