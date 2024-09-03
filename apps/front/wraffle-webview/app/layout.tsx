256dcea74cc0981d890959610a9583e836e59382
sangminlee98 <tkdals0975@naver.com>
2024-09-03 21:47:37 +0900

    [WRFE-13](feat): 로그인 페이지

--- apps/front/wraffle-webview/app/layout.tsx
+++ apps/front/wraffle-webview/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body className={inter.className}>
-        <div className='h-screen min-h-screen'>
+        <div className='container h-screen min-h-screen'>
           {IconLoader}
           {children}
         </div>
