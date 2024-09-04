6003c3a2b8706e70c868524be387ed8ea5f171a7
sangminlee98 <tkdals0975@naver.com>
2024-09-04 21:05:14 +0900

    [WRFE-13](feat): 이메일 로그인 페이지 구현

--- packages/ui/src/ui/form/input/Input.tsx
+++ packages/ui/src/ui/form/input/Input.tsx
@@ -3,7 +3,7 @@ import * as React from 'react';
 import {cn} from '@wds/shared/utils';
 
 const inputVariants = cva(
-  'flex h-[55px] max-w-[335px] w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50',
+  'flex h-[55px] w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50',
   {
     variants: {
       disabled: {
