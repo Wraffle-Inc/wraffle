886272129dcdf09cd1221e511b1af16b49f60e29
hwang <hgh1057@gmail.com>
2024-09-04 16:58:45 +0900

    [WRFE-14](fix): react query 수정

--- apps/front/wraffle-webview/src/shared/hook/useReactQuery.tsx
+++ apps/front/wraffle-webview/src/shared/hook/useReactQuery.tsx
@@ -1,23 +1,37 @@
 'use client';
 
-import {useState} from 'react';
-import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
+import type {ReactNode} from 'react';
+import {
+  isServer,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
 
-const ReactQueryProviders = ({children}: React.PropsWithChildren) => {
-  const [queryClient] = useState(
-    () =>
-      new QueryClient({
-        defaultOptions: {
-          queries: {
-            staleTime: 60 * 1000,
-          },
-        },
-      }),
-  );
+function makeQueryClient() {
+  return new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 60 * 1000,
+      },
+    },
+  });
+}
+
+let browserQueryClient: QueryClient | undefined = undefined;
+
+function getQueryClient() {
+  if (isServer) {
+    return makeQueryClient();
+  } else {
+    if (!browserQueryClient) browserQueryClient = makeQueryClient();
+    return browserQueryClient;
+  }
+}
+
+export default function ReactQueryProviders({children}: {children: ReactNode}) {
+  const queryClient = getQueryClient();
 
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
   );
-};
-
-export default ReactQueryProviders;
+}
