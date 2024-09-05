e23bf24c636486fe79740616d0aeecdfd7381637
hwang <hgh1057@gmail.com>
2024-09-05 11:18:12 +0900

    # [WRDS-22](fix): stories 형식 변경 및 Calendar export추가

--- packages/ui/src/ui/calendar/Calendar.stories.tsx
+++ packages/ui/src/ui/calendar/Calendar.stories.tsx
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from '@storybook/react';
+import type { Meta, StoryFn } from '@storybook/react';
 import CalendarForm from './Calendar';
 import { useState } from 'react';
 
@@ -9,24 +9,16 @@ const meta = {
 
 export default meta;
 
-type Story = StoryObj<typeof meta>;
-
-const CalendarForRender = (args: any) => {
+const Template: StoryFn<typeof CalendarForm> = (args) => {
   const [selected, setSelected] = useState<Date | undefined>();
-
   return (
     <CalendarForm {...args} selected={selected} setSelected={setSelected} />
   );
 };
 
-export const CalendarFormDefault: Story = {
-  name: 'CalendarForm',
-  args: {
-    formLabel: '어떤 일정을 정할지에 대해 작성하는 라벨입니다.',
-    className: '',
-    dateLabel: '선택한 날짜가 표시되기 전 보여지는 라벨입니다',
-    selected: undefined,
-    setSelected: () => {},
-  },
-  render: (args) => <CalendarForRender {...args} />,
+export const CalendarFormDefault = Template.bind({});
+CalendarFormDefault.args = {
+  formLabel: '어떤 일정을 정할지에 대해 작성하는 라벨입니다.',
+  className: '',
+  dateLabel: '선택한 날짜가 표시되기 전 보여지는 라벨입니다',
 };
