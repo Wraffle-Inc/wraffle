e36e07c343b35facab72da0ef1f87cf0afed3436
sangminlee98 <tkdals0975@naver.com>
2024-09-16 21:13:46 +0900

    [WRFE-13](fix): 디자인 시스템에서 발생하는 버그 수정

--- packages/ui/src/ui/form/label/Label.tsx
+++ packages/ui/src/ui/form/label/Label.tsx
@@ -6,7 +6,7 @@ import * as LabelPrimitive from '@radix-ui/react-label';
 import {cn} from '@wds/shared/utils';
 
 const labelVariants = cva(
-  'text-sm font-medium mt-96 leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70',
+  'text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70',
 );
 
 const Label = React.forwardRef<
