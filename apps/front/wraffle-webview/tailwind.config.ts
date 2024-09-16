110b353959e8913a78453ffa2a784186267b2270
sangminlee98 <tkdals0975@naver.com>
2024-09-16 22:28:01 +0900

    [WRFE-13](feat): local font로 Pretendard 폰트 전역 설정

--- apps/front/wraffle-webview/tailwind.config.ts
+++ apps/front/wraffle-webview/tailwind.config.ts
@@ -2,9 +2,14 @@
 import type {Config} from 'tailwindcss';
 import sharedConfig from '@wraffle/tailwind-config';
 
-const config: Pick<Config, 'content' | 'presets'> = {
+const config: Pick<Config, 'content' | 'presets' | 'extends'> = {
   content: ['**/*.{ts,tsx}', '../../packages/ui/src/**/*.{ts,tsx}'],
   presets: [sharedConfig],
+  extends: {
+    fontFamily: {
+      pretendard: ['var(--font-pretendard)'],
+    },
+  },
 };
 
 export default config;
