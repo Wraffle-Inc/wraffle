5d820c8adf8cf86d520d36980a50263e5432be29
gyeong3un2 <gyeong3un2@gmail.com>
2024-09-25 00:04:03 +0900

    [WRFE-16](feat): 비밀번호 찾기 페이지 UI 구현

--- apps/front/wraffle-webview/app/layout.tsx
+++ apps/front/wraffle-webview/app/layout.tsx
@@ -16,7 +16,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className='h-screen min-h-screen'>{children}</div>
+      </body>
     </html>
   );
 }
