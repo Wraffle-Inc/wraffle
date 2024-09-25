e4848d513a564093fa3d57af2051938420a6365e
ww8007 <wshmin1234@gmail.com>
2024-09-26 00:07:25 +0900

    [WRFE-17](setting): expo router를 turbo repo 환경에서 동작할 수 있게 설정 변경

--- apps/front/wraffle-native/src/shared/hooks/useThemeColor.ts
+++ apps/front/wraffle-native/src/shared/hooks/useThemeColor.ts
@@ -2,16 +2,14 @@
  * Learn more about light and dark modes:
  * https://docs.expo.dev/guides/color-schemes/
  */
-
-import { useColorScheme } from "react-native";
-
-import { Colors } from "@/constants/Colors";
+import {Colors} from '../ui/constants/Colors';
+import {useColorScheme} from 'react-native';
 
 export function useThemeColor(
-  props: { light?: string; dark?: string },
+  props: {light?: string; dark?: string},
   colorName: keyof typeof Colors.light & keyof typeof Colors.dark,
 ) {
-  const theme = useColorScheme() ?? "light";
+  const theme = useColorScheme() ?? 'light';
   const colorFromProps = props[theme];
 
   if (colorFromProps) {
