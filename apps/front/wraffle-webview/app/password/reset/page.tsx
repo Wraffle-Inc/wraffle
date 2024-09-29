d41574c4a6df49502475f566cfba64c0c74b6207
gyeong3un2 <gyeong3un2@gmail.com>
2024-09-29 18:07:34 +0900

    [WRFE-16](style): 다른 페이지에 BottomFixedBox 컴포넌트 적용

--- apps/front/wraffle-webview/app/password/reset/page.tsx
+++ apps/front/wraffle-webview/app/password/reset/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import {useRouter} from 'next/navigation';
+import BottomFixedBox from '@/shared/ui/bottom/BottomFixedBox';
 import {Header} from '@/shared/ui/header/core/Header';
 import {Button, Input, Typography} from '@wraffle/ui';
 
@@ -16,11 +17,9 @@ function ResetPasswordPage() {
       <Header>
         <Header.BackButton onClick={router.back} />
       </Header>
-      <div
-        className='container flex flex-col justify-between'
-        style={{height: 'calc(100dvh - 60px)'}}
-      >
-        <section className='flex-col justify-start'>
+
+      <div className='container'>
+        <div>
           <Typography className='my-5 text-h2'>새로운 비밀번호 입력</Typography>
 
           <Typography className='text-p4'>새 비밀번호</Typography>
@@ -28,11 +27,11 @@ function ResetPasswordPage() {
             className='my-1.5'
             placeholder='새로운 비밀번호를 입력해주세요.'
           />
-        </section>
+        </div>
 
-        <section className='flex-col justify-end'>
+        <BottomFixedBox>
           <Button onClick={handleResetPassword}>비밀번호 재설정하기</Button>
-        </section>
+        </BottomFixedBox>
       </div>
     </div>
   );
