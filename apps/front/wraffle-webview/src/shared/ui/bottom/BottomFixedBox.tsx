d62edde3143f8aa0c188b5535c856f6efca0bb3c
gyeong3un2 <gyeong3un2@gmail.com>
2024-09-29 18:07:34 +0900

    [WRFE-16](style): 다른 페이지에 BottomFixedBox 컴포넌트 적용

--- apps/front/wraffle-webview/src/shared/ui/bottom/BottomFixedBox.tsx
+++ apps/front/wraffle-webview/src/shared/ui/bottom/BottomFixedBox.tsx
@@ -1,8 +1,16 @@
 import type {PropsWithChildren} from 'react';
 
-function BottomFixedBox({children}: PropsWithChildren) {
+interface BottomFixedBoxProps extends PropsWithChildren {
+  className?: string;
+}
+
+function BottomFixedBox({children, className}: BottomFixedBoxProps) {
   return (
-    <div className='fixed bottom-0 left-0 right-0 mb-4 px-8'>{children}</div>
+    <div
+      className={`fixed bottom-0 left-0 right-0 mb-4 px-8 ${className || ''}`}
+    >
+      {children}
+    </div>
   );
 }
 
