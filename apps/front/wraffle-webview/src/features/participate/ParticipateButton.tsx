5e97be5dc073fe08a506efad606bf66f19b12c34
ajeong <jenny2360@tukorea.ac.kr>
2024-10-02 02:10:04 +0900

    [WRFE-10](feat): applyStatus 추가에 대한 버튼 상태 변경

--- apps/front/wraffle-webview/src/features/participate/ParticipateButton.tsx
+++ apps/front/wraffle-webview/src/features/participate/ParticipateButton.tsx
@@ -6,11 +6,13 @@ import {Button, Icon} from '@wraffle/ui';
 interface ParticipateButtonProps {
   status: string;
   clipCount: number;
+  applyStatus: boolean;
 }
 
 const ParticipateButton: React.FC<ParticipateButtonProps> = ({
   status,
   clipCount,
+  applyStatus,
 }) => {
   const [isBookmarked, setIsBookmarked] = useState(false);
 
@@ -27,14 +29,18 @@ const ParticipateButton: React.FC<ParticipateButtonProps> = ({
   }
 
   return (
-    <div className='flex gap-4 p-4'>
+    <div className='flex gap-4 p-2'>
       <button onClick={handleBookmark}>
         <div className='flex flex-col items-center justify-center'>
           <Icon name='bookmark' color={isBookmarked ? 'black' : ''} />
           <span>{isBookmarked ? `${clipCount + 1}` : `${clipCount}`}</span>
         </div>
       </button>
-      <Button variant='default'>응모하기</Button>
+      {applyStatus ? (
+        <Button variant='gray'>응모를 완료하였습니다</Button>
+      ) : (
+        <Button variant='default'>응모하기</Button>
+      )}
     </div>
   );
 };
