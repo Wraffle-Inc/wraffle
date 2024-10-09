9d2c1b464d6407a0d725a48277556abab919f96a
ajeong <jenny2360@tukorea.ac.kr>
2024-10-09 20:38:09 +0900

    [WRFE-10](feat): 쿼리스트링 값에 따라 raffle/event 페이지 생성

--- apps/front/wraffle-webview/src/features/product-menu/useMenu.ts
+++ apps/front/wraffle-webview/src/features/product-menu/useMenu.ts
@@ -1,6 +1,6 @@
 import {useState} from 'react';
 
-type MenuOption = '상품' | '응모 기간' | '당첨자 발표' | '유의사항';
+type MenuOption = string;
 
 export const useMenu = (initialMenu: MenuOption) => {
   const [selectedMenu, setSelectedMenu] = useState<MenuOption>(initialMenu);
