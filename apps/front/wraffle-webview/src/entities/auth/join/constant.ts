349770261f0a2705bf5405fda847cbc27f04d00a
sangminlee98 <tkdals0975@naver.com>
2024-10-15 22:03:13 +0900

    [WRFE-18](feat): 약관 동의 스텝 구현

---
+++ apps/front/wraffle-webview/src/entities/auth/join/constant.ts
@@ -0,0 +1,18 @@
+export const terms = [
+  {
+    id: 'isAgreed',
+    label: '이용약관에 동의(필수)',
+  },
+  {
+    id: 'isPrivacyAgreed',
+    label: '개인정보 취급방침 동의(필수)',
+  },
+  {
+    id: 'isThirdAgreed',
+    label: '제3자 동의(필수)',
+  },
+  {
+    id: 'isMarketingAgreed',
+    label: '마케팅 약관 동의(선택)',
+  },
+] as const;
