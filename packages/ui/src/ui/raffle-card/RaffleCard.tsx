d8309d29247b25bce5f3045c0cec6fddfbc5f7f5
ww8007 <wshmin1234@gmail.com>
2024-10-18 00:51:37 +0900

    [WRDS-24](fix): 컴포넌트 변경 사항에 따른 변경사항 반영

--- packages/ui/src/ui/raffle-card/RaffleCard.tsx
+++ packages/ui/src/ui/raffle-card/RaffleCard.tsx
@@ -34,27 +34,23 @@ const RaffleCard = ({
         />
         {isClosed && (
           <div className='absolute z-10 flex h-full w-full items-center justify-center rounded-sm bg-black bg-opacity-40'>
-            <Typography className='text-white' fontWeight='bold'>
+            <Typography size='h5' color='white'>
               마감되었어요 🫠
             </Typography>
           </div>
         )}
       </div>
-      <Typography className='truncate text-[14px]' fontWeight='semibold'>
+      <Typography className='truncate' size='p2'>
         {name}
       </Typography>
-      <Typography
-        className='mb-2 text-[12px] text-gray-600'
-        fontSize='sm2'
-        fontWeight='semibold'
-      >
+      <Typography className='mb-2' size='sm2' color='zinc600'>
         {price}원
       </Typography>
       <VisibleTags hashtags={hashtags} letterSpace={20} />
       <span className='flex items-center justify-start gap-0.5'>
         {isBookmarked && <Icon name='bookmark' color='black' />}
         {!isBookmarked && <Icon name='bookmark' />}
-        <Typography className='text-[11px]' fontWeight='semibold'>
+        <Typography size='sm1' color='zinc600'>
           {scrapCount.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
         </Typography>
       </span>
