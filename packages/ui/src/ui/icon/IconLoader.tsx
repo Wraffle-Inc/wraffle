96068c937489227faddf6ddf73e15b1dc97e5928
gyeong3un2 <gyeong3un2@gmail.com>
2024-10-23 00:34:22 +0900

    [WRFE-43](design): divider, 인스타 아이콘 추가

--- packages/ui/src/ui/icon/IconLoader.tsx
+++ packages/ui/src/ui/icon/IconLoader.tsx
@@ -23,9 +23,9 @@ const IconLoader = (
     <symbol id='chevron-right' viewBox='0 0 7 12'>
       <path
         stroke='currentColor'
-        stroke-linecap='round'
-        stroke-linejoin='round'
-        stroke-width='1.5'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+        strokeWidth='1.5'
         d='M1 11l5-5-5-5'
       />
     </symbol>
