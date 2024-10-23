45d76614f4cba21aa48751dbb2065854c97a40b6
hwang <hgh1057@gmail.com>
2024-10-24 02:01:10 +0900

    [WRFE-15](feat): 리스트 인덱스 추가

---
+++ apps/front/wraffle-webview/src/widgets/product-list/create/ui/index.ts
@@ -0,0 +1,6 @@
+export {DateStep} from './DateStepList';
+export {ImageStep} from './ImageStepList';
+export {EtcStep} from './EtcStepList';
+export {TitleStep} from './TitleStepList';
+export {ProductList} from './ProductList';
+export {SuccessList} from './SuccessList';
