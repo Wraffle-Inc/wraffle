d78219e24639393e6f6856c479f7430b4883e3d0
gyeong3un2 <gyeong3un2@gmail.com>
2024-10-23 22:45:04 +0900

    [WRFE-43](style): Typography className 설정한 코드 제거

--- apps/front/wraffle-webview/app/my-profile/page.tsx
+++ apps/front/wraffle-webview/app/my-profile/page.tsx
@@ -1,32 +1,37 @@
+'use client';
+
+import {useRouter} from 'next/navigation';
 import {Header} from '@/shared/ui';
-import Footer from '@/widgets/my-profile/ui/Footer';
 import {DualInfoBox} from '@/widgets/my-profile/ui/InfoBox';
 import {Icon, Typography} from '@wraffle/ui';
-import BottomNavigation from '@wraffle/ui/src/ui/bottomNavigation/BottomNavigation';
 import IconWithLabel from '@wraffle/ui/src/ui/iconWithLabel/IconWithLabel';
 
-function MyProfilePage() {
+const MyProfilePage = () => {
+  const router = useRouter();
+
+  const handleSettlement = () => {
+    router.push('/my-profile/settlement');
+  };
+
   return (
     <div>
       <Header>
         <Header.Middle>마이페이지</Header.Middle>
       </Header>
 
-      <div className='mt-5 flex flex-col'>
-        <Typography size='h2'>홍길동님</Typography>
+      <div className='flex flex-col py-8'>
+        <div className='px-8'>
+          <Typography size='h2'>홍길동님</Typography>
+        </div>
 
-        <div className='mt-1 flex items-center'>
-          <Typography
-            size='h6'
-            color='zinc500'
-            className='mr-1 pt-1 font-normal'
-          >
+        <div className='mt-1 flex items-center px-8 hover:cursor-pointer'>
+          <Typography size='p3' color='zinc500'>
             내정보 수정
           </Typography>
           <Icon name='chevron-right' stroke='#4E5968' width={14} height={14} />
         </div>
 
-        <div className='mt-6'>
+        <div className='mt-6 px-8'>
           <DualInfoBox
             leftLabel='적립금'
             leftValue='1,000원'
@@ -35,7 +40,7 @@ function MyProfilePage() {
           />
         </div>
 
-        <div className='mx-5 mt-4 flex h-16 items-center justify-between'>
+        <div className='mx-5 mt-4 flex h-16 items-center justify-between px-8'>
           <IconWithLabel Icon={<Icon name='gift' />} label='내 래플' />
           <IconWithLabel Icon={<Icon name='credit-card' />} label='카드 관리' />
           <IconWithLabel
@@ -45,33 +50,31 @@ function MyProfilePage() {
           <IconWithLabel Icon={<Icon name='bookmark' />} label='스크랩' />
         </div>
 
-        {/* TODO: container css에 padding이 들어가있어서 w-full이 안됨 */}
         <div className='mt-4 h-2.5 border border-zinc-200 bg-zinc-100' />
 
-        <ul>
-          <li className='flex h-14 items-center border-b-2 border-b-zinc-100'>
-            <Typography size='h6' color='zinc700' className='font-normal'>
+        <ul className='px-8'>
+          <li
+            className='flex h-14 items-center border-b-2 border-b-zinc-100 hover:cursor-pointer'
+            onClick={handleSettlement}
+          >
+            <Typography size='p3' color='zinc700'>
               내 정산금 관리
             </Typography>
           </li>
-          <li className='flex h-14 items-center border-b-2 border-b-zinc-100'>
-            <Typography size='h6' color='zinc700' className='font-normal'>
+          <li className='flex h-14 items-center border-b-2 border-b-zinc-100 hover:cursor-pointer'>
+            <Typography size='p3' color='zinc700'>
               공지사항
             </Typography>
           </li>
-          <li className='flex h-14 items-center border-b-2 border-b-zinc-100'>
-            <Typography size='h6' color='zinc700' className='font-normal'>
+          <li className='flex h-14 items-center border-b-2 border-b-zinc-100 hover:cursor-pointer'>
+            <Typography size='p3' color='zinc700'>
               로그아웃
             </Typography>
           </li>
         </ul>
       </div>
-
-      <Footer />
-
-      <BottomNavigation />
     </div>
   );
-}
+};
 
 export default MyProfilePage;
