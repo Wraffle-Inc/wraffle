a950f3f7add2478cb481c00d6e9dfb195e31f903
hwang <hgh1057@gmail.com>
2024-10-24 02:40:35 +0900

    [WRFE-15](refactor): Progress 중괄호 제거

--- apps/front/wraffle-webview/src/shared/ui/progress/Progress.tsx
+++ apps/front/wraffle-webview/src/shared/ui/progress/Progress.tsx
@@ -8,7 +8,7 @@ export const ProgressBar = ({
   const progress = ((index + 1) / totalSteps) * 100;
 
   return (
-    <div className={'h-0.5 w-full bg-[#F4F4F5]'}>
+    <div className='h-0.5 w-full bg-[#F4F4F5]'>
       <div
         style={{
           width: `${progress}%`,
