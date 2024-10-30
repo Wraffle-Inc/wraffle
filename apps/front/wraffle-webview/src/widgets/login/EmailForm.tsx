2c8b01411aad336666ab11c53c4e03913b7114ef
sangminlee98 <tkdals0975@naver.com>
2024-10-30 18:02:48 +0900

    [WRFE-18](fix): 로그인 페이지 Typography 수정

--- apps/front/wraffle-webview/src/widgets/login/EmailForm.tsx
+++ apps/front/wraffle-webview/src/widgets/login/EmailForm.tsx
@@ -1,7 +1,7 @@
 import {useFormContext} from 'react-hook-form';
 import type {LoginPayload} from '@/entities/auth';
 import {RHFInput} from '@/shared/ui';
-import {Button} from '@wraffle/ui';
+import {Button, Typography} from '@wraffle/ui';
 
 const EmailForm = () => {
   const {formState} = useFormContext<LoginPayload>();
@@ -24,7 +24,9 @@ const EmailForm = () => {
         type='submit'
         disabled={!formState.isValid}
       >
-        로그인
+        <Typography size='h6' color='white'>
+          로그인
+        </Typography>
       </Button>
     </>
   );
