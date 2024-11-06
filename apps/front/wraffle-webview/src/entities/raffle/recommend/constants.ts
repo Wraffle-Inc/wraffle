5ee963602131cd28e5a475c3558670c7dbb13df8
sangminlee98 <tkdals0975@naver.com>
2024-11-06 20:07:21 +0900

    [WRFE-55](feat): 최신 래플 목록 UI 구현

---
+++ apps/front/wraffle-webview/src/entities/raffle/recommend/constants.ts
@@ -0,0 +1,84 @@
+import type {RecommendRaffle} from './type';
+
+export const recommendItems: RecommendRaffle[] = [
+  {
+    id: 1,
+    type: 'RAFFLE',
+    title: '[Vans] 올드스쿨',
+    price: 78000,
+    clipCount: 2,
+    hashtags: [
+      {
+        id: 1,
+        name: '한정판',
+      },
+      {
+        id: 2,
+        name: 'Vans',
+      },
+    ],
+    thumbnail:
+      'https://image.vans.co.kr/cmsstatic/product/VN000D3HY281_VN000D3HY281_primary.jpg',
+    isBookmarked: false,
+  },
+  {
+    id: 2,
+    type: 'RAFFLE',
+    title: '[Vans] 올드스쿨',
+    price: 78000,
+    clipCount: 2,
+    hashtags: [
+      {
+        id: 1,
+        name: '한정판',
+      },
+      {
+        id: 2,
+        name: 'Vans',
+      },
+    ],
+    thumbnail:
+      'https://image.vans.co.kr/cmsstatic/product/VN000D3HY281_VN000D3HY281_primary.jpg',
+    isBookmarked: false,
+  },
+  {
+    id: 3,
+    type: 'RAFFLE',
+    title: '[Vans] 올드스쿨',
+    price: 78000,
+    clipCount: 2,
+    hashtags: [
+      {
+        id: 1,
+        name: '한정판',
+      },
+      {
+        id: 2,
+        name: 'Vans',
+      },
+    ],
+    thumbnail:
+      'https://image.vans.co.kr/cmsstatic/product/VN000D3HY281_VN000D3HY281_primary.jpg',
+    isBookmarked: false,
+  },
+  {
+    id: 4,
+    type: 'RAFFLE',
+    title: '[Vans] 올드스쿨',
+    price: 78000,
+    clipCount: 2,
+    hashtags: [
+      {
+        id: 1,
+        name: '한정판',
+      },
+      {
+        id: 2,
+        name: 'Vans',
+      },
+    ],
+    thumbnail:
+      'https://image.vans.co.kr/cmsstatic/product/VN000D3HY281_VN000D3HY281_primary.jpg',
+    isBookmarked: false,
+  },
+];
---
+++ apps/front/wraffle-webview/src/widgets/recommend-list/ui/RecommendList.tsx
@@ -0,0 +1,30 @@
+import type {RecommendRaffle} from '@/entities/raffle/recommend/type';
+import {RaffleCard, Typography} from '@wraffle/ui';
+
+interface RecommendListProps {
+  recommendItems: RecommendRaffle[];
+}
+
+const RecommendList = ({recommendItems}: RecommendListProps) => {
+  return (
+    <section className='w-full max-w-screen-md p-3'>
+      <Typography as='h1' size='h2'>
+        이번주 추천 래플
+      </Typography>
+      <div className='grid grid-cols-[repeat(auto-fill,minmax(160px,1fr))] justify-items-center gap-4'>
+        {recommendItems.map(item => (
+          <RaffleCard
+            name={item.title}
+            price={item.price}
+            hashtags={item.hashtags}
+            scrapCount={item.clipCount}
+            thumbnailUrl={item.thumbnail}
+            isBookmarked={item.isBookmarked}
+          />
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default RecommendList;
