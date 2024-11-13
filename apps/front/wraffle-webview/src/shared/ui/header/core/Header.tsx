4efe0e4c1e33723bccbcd863ef3f942747a8b1b9
woohee <aldkgha@naver.com>
2024-11-13 20:44:11 +0900

    [WRFE-11](fix): conflict 해결

---
+++ apps/front/wraffle-webview/src/shared/ui/header/core/Header.tsx
@@ -0,0 +1,77 @@
+import Image from 'next/image';
+import {type ReactNode} from 'react';
+import {Icon} from '@wraffle/ui/src/ui/icon/Icon';
+import {Typography} from '@wraffle/ui/src/ui/typography/Typography';
+
+interface HeaderProps {
+  withBackButton?: boolean;
+  children: ReactNode;
+}
+
+const HeaderPrimitive = ({children}: HeaderProps) => {
+  return (
+    <div className='flex h-[52px] items-center justify-between px-5'>
+      {children}
+    </div>
+  );
+};
+
+type WithChildren = {
+  children: ReactNode;
+};
+
+const Left = ({children}: WithChildren) => {
+  return <div className='flex flex-1 items-center gap-4'>{children}</div>;
+};
+
+const Middle = ({children}: WithChildren) => {
+  return <div className='flex flex-1 items-center gap-4'>{children}</div>;
+};
+
+const Right = ({children}: WithChildren) => {
+  return <div className='flex flex-1 items-center gap-4'>{children}</div>;
+};
+
+interface BackButtonProps {
+  onClick: () => void;
+}
+
+const BackButton = ({onClick}: BackButtonProps) => {
+  return (
+    <button
+      onClick={onClick}
+      className='flex items-center justify-center text-black'
+    >
+      <Icon name='arrow-left' />
+    </button>
+  );
+};
+
+const Logo = () => {
+  return (
+    <div className='flex items-center justify-center'>
+      <Image src='/logo.png' alt='logo' width={100} height={100} />
+    </div>
+  );
+};
+
+interface TitleProps {
+  children: string;
+}
+
+const Title = ({children}: TitleProps) => {
+  return (
+    <Typography size='h1' as='h1' color='black'>
+      {children}
+    </Typography>
+  );
+};
+
+HeaderPrimitive.Left = Left;
+HeaderPrimitive.Middle = Middle;
+HeaderPrimitive.Right = Right;
+HeaderPrimitive.BackButton = BackButton;
+HeaderPrimitive.Logo = Logo;
+HeaderPrimitive.Title = Title;
+
+export {HeaderPrimitive as Header};
