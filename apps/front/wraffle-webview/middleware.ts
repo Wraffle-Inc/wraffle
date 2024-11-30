d7cc279b3a0372059c909338c71c0461bc934349
sangminlee98 <tkdals0975@naver.com>
2024-11-30 19:00:07 +0900

    [WRFE-61](feat): middleware를 사용하여 Private Route 처리

---
+++ apps/front/wraffle-webview/middleware.ts
@@ -0,0 +1,32 @@
+import type {NextRequest} from 'next/server';
+import {NextResponse} from 'next/server';
+import {auth} from '@/shared/util/auth';
+
+const PUBLIC_ROUTES = ['/login', '/join'];
+const LOGIN = '/login';
+const MAIN = '/';
+
+export async function middleware(req: NextRequest) {
+  const {nextUrl} = req;
+  const session = await auth();
+
+  const isAuthenticated = !!session?.user;
+
+  const isPublicRoute = PUBLIC_ROUTES.find(route =>
+    nextUrl.pathname.startsWith(route),
+  );
+
+  if (isAuthenticated && isPublicRoute) {
+    return NextResponse.redirect(new URL(MAIN, req.url));
+  }
+
+  if (!isAuthenticated && !isPublicRoute) {
+    return NextResponse.redirect(new URL(LOGIN, nextUrl));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ['/'],
+};
