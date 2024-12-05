39027770f4944176392da03121c26cc937303436
dongmin <dongmin11566@gmail.com>
2024-12-05 16:54:13 +0900

    [WRFE-63](design): Icon 컴포넌트로 아이콘 변경

--- apps/admin/app/layout.tsx
+++ apps/admin/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css';
 import type {Metadata} from 'next';
 import {Inter} from 'next/font/google';
+import IconLoader from '@wraffle/ui/src/ui/icon/IconLoader';
 
 const inter = Inter({subsets: ['latin']});
 
@@ -16,7 +17,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {IconLoader}
+        {children}
+      </body>
     </html>
   );
 }
