f0692dbf31f00129eed2b9307786ba1f792ab79d
dongmin <dongmin11566@gmail.com>
2024-12-12 15:36:06 +0900

    [WRFE-63](design): SideBarTab 의 화살표 아이콘 변경 및 활성화 상태에 따라서 조건부 렌더링 하도록 변경

--- apps/admin/app/@sidebar/_components/SideBarTab.tsx
+++ apps/admin/app/@sidebar/_components/SideBarTab.tsx
@@ -12,19 +12,26 @@ export default function SideBarTab({
   href: string;
 }) {
   const path = usePathname();
+  const isActive = href === path;
   return (
     <Link href={href}>
-      <div className='flex flex-row items-center justify-between'>
+      <div className='group flex flex-row items-center justify-between'>
         <p
           className={
-            path === href
-              ? 'p-2 font-bold text-[#4E5968]'
-              : 'p-2 text-[#4E5968]'
+            isActive ? 'p-2 font-bold text-[#4E5968]' : 'p-2 text-[#4E5968]'
           }
         >
           {children}
         </p>
-        <Icon name='arrow-bottom' stroke='#B0B8C1' width={12} height={12} />
+        <Icon
+          name='arrow-right'
+          stroke='#B0B8C1'
+          width={12}
+          height={12}
+          className={`transition-opacity duration-300 ${
+            isActive ? 'opacity-100' : 'opacity-0 group-hover:opacity-100'
+          }`}
+        />
       </div>
     </Link>
   );
