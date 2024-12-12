e351e08b277840eac173ae01bf3573bd83d48149
dongmin <dongmin11566@gmail.com>
2024-12-12 15:16:30 +0900

    [WRFE-63](feat): 활성화된 탭에 대하여 bold 처리

--- apps/admin/app/@sidebar/_components/SideBarTab.tsx
+++ apps/admin/app/@sidebar/_components/SideBarTab.tsx
@@ -1,4 +1,7 @@
+'use client';
+
 import Link from 'next/link';
+import {usePathname} from 'next/navigation';
 import {Icon} from '@wraffle/ui';
 
 export default function SideBarTab({
@@ -8,12 +11,22 @@ export default function SideBarTab({
   children: React.ReactNode;
   href: string;
 }) {
+  const path = usePathname();
   return (
     <Link href={href}>
       <div className='flex flex-row items-center justify-between'>
-        <p className='p-2 text-[#4E5968]'>{children}</p>
+        <p
+          className={
+            path === href
+              ? 'p-2 font-bold text-[#4E5968]'
+              : 'p-2 text-[#4E5968]'
+          }
+        >
+          {children}
+        </p>
         <Icon name='arrow-bottom' stroke='#B0B8C1' width={12} height={12} />
       </div>
     </Link>
   );
 }
+`p-2 text-[#4E5968]`;
