0827dec17b54ea17840a5240db65f4bfa165dedd
sangminlee98 <tkdals0975@naver.com>
2024-12-31 02:15:19 +0900

    [WRFE-74](chore): next.js eslint-plugin-react-hooks 린트 플러그인 추가 설정

--- packages/shared/eslint-config/next.js
+++ packages/shared/eslint-config/next.js
@@ -7,6 +7,7 @@ module.exports = {
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
     "prettier",
     require.resolve("@vercel/style-guide/eslint/next"),
     "turbo",
