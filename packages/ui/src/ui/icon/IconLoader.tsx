fef680b69877f0e354d0c7b8bd01cc30fd730a67
sangminlee98 <tkdals0975@naver.com>
2025-02-12 20:03:41 +0900

    [WRFE-79](fix): 디자인 시스템 컴포넌트 오류 수정

--- packages/ui/src/ui/icon/IconLoader.tsx
+++ packages/ui/src/ui/icon/IconLoader.tsx
@@ -111,19 +111,14 @@ const IconLoader = (
       />
     </symbol>
 
-    <svg
-      id='plus'
-      width='14'
-      height='14'
-      viewBox='0 0 14 14'
-      fill='none'
-      xmlns='http://www.w3.org/2000/svg'
-    >
+    <symbol id='plus' viewBox='0 0 14 14'>
       <path
+        stroke='currentColor'
+        strokeWidth='0.5'
+        fill='currentColor'
         d='M14 7.99805H8V13.998H6V7.99805H0V5.99805H6V-0.00195312H8V5.99805H14V7.99805Z'
-        fill='black'
       />
-    </svg>
+    </symbol>
 
     <symbol id='bell' viewBox='0 0 24 24'>
       <path
