ff4d8bd836a467f47a1e2de7ff1bd6142ac5498f
dongmin <dongmin11566@gmail.com>
2025-03-01 19:06:31 +0900

    [WRFE-85](design): 회원관리 페이지 헤더 추가

--- apps/admin/app/@content/users/_components/UserListHeader/index.tsx
+++ apps/admin/app/@content/users/_components/UserListHeader/index.tsx
@@ -1,5 +1,11 @@
 const UserListHeader = () => {
-  return <div>UserListHeader</div>;
+  return (
+    <div className='w-full p-[22px]'>
+      <h2 className='text-[24px] font-bold leading-normal text-brand-100'>
+        회원관리
+      </h2>
+    </div>
+  );
 };
 
 export default UserListHeader;
