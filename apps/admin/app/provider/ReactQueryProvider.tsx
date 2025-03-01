da8a9f807d541798a0ccca9c8087c26545686d45
dongmin <dongmin11566@gmail.com>
2025-03-01 19:04:12 +0900

    [WRFE-85](test): react-query devtools 적용

--- apps/admin/app/provider/ReactQueryProvider.tsx
+++ apps/admin/app/provider/ReactQueryProvider.tsx
@@ -2,6 +2,7 @@
 
 import {ReactNode, useState} from 'react';
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
+import {ReactQueryDevtools} from '@tanstack/react-query-devtools';
 
 interface Props {
   children: ReactNode;
@@ -11,6 +12,9 @@ export default function ReactQueryProvider({children}: Props) {
   const [queryClient] = useState(() => new QueryClient());
 
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={true} />
+      {children}
+    </QueryClientProvider>
   );
 }
