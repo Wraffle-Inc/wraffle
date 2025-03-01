385b73d301b452a2b69bb50e494a4ce386315e86
dongmin <dongmin11566@gmail.com>
2025-03-01 19:11:20 +0900

    [WRFE-85](feat): 유저 리스트 보드 콘텐츠 추가

--- apps/admin/app/@content/users/_clientBoundary/UserListClient/index.tsx
+++ apps/admin/app/@content/users/_clientBoundary/UserListClient/index.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import UserListBoardContents from '../../_components/UserListBoardContents';
 import userListQuery from '@/api/userList/query';
 import UserListHeader from '@/app/@content/users/_components/UserListHeader';
 import ListBoardHeader from '@/app/_components/ListBoardHeader';
@@ -16,6 +17,7 @@ const UserListClient = () => {
         labels={['이름', '가입일', '계정', '생성한 래플 수']}
         isCheckBox={true}
       />
+      <UserListBoardContents isCheckBox={true} contents={data} />
     </div>
   );
 };
