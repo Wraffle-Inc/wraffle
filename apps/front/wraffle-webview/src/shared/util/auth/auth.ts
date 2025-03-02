7c7fde139ab1f4ea31fddab10674371696978776
sangminlee98 <tkdals0975@naver.com>
2025-03-02 15:38:21 +0900

    [WRFE-87](fix): 재발급 함수 public 요청으로 수정

--- apps/front/wraffle-webview/src/shared/util/auth/auth.ts
+++ apps/front/wraffle-webview/src/shared/util/auth/auth.ts
@@ -37,7 +37,6 @@ export const {handlers, signIn, signOut, auth} = NextAuth({
               body: {email, password},
             },
           );
-
           return response.data;
         } catch (error) {
           const credentialsSignin = new CredentialsSignin();
