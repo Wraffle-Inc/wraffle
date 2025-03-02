9bd38d95afc88d42eb6263bf94eed48710e304fa
dongmin <dongmin11566@gmail.com>
2025-03-02 21:36:52 +0900

    [WRFE-85](refactor): labels 재생성 방지하도록 컴포넌트 밖에서 관리

--- apps/admin/app/@content/users/_clientBoundary/UserListClient/index.tsx
+++ apps/admin/app/@content/users/_clientBoundary/UserListClient/index.tsx
@@ -5,6 +5,8 @@ import userListQuery from '@/api/userList/query';
 import UserListHeader from '@/app/@content/users/_components/UserListHeader';
 import ListBoardHeader from '@/app/_components/ListBoardHeader';
 
+const labels = ['이름', '가입일', '계정', '생성한 래플 수'];
+
 const UserListClient = () => {
   const {data} = userListQuery();
 
@@ -13,10 +15,7 @@ const UserListClient = () => {
   return (
     <div className='w-full'>
       <UserListHeader />
-      <ListBoardHeader
-        labels={['이름', '가입일', '계정', '생성한 래플 수']}
-        isCheckBox={true}
-      />
+      <ListBoardHeader labels={labels} isCheckBox={true} />
       <UserListBoardContents isCheckBox={true} contents={data} />
     </div>
   );
