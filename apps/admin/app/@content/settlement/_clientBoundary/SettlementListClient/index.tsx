df326a22348da35fe9604d9bce1a4af968c1e68f
dongmin <dongmin11566@gmail.com>
2025-03-03 03:29:20 +0900

    [WRFE-89](feat): 정산관리 리스트 조회

--- apps/admin/app/@content/settlement/_clientBoundary/SettlementListClient/index.tsx
+++ apps/admin/app/@content/settlement/_clientBoundary/SettlementListClient/index.tsx
@@ -1,7 +1,11 @@
 'use client';
 
+import SettlementListBoardContents from '../../_components/SettlementListBoardContents';
+import ListBoardHeader from '@/app/_components/ListBoardHeader';
 import Title from '@/app/_components/Title';
 import settlementListQuery from '@/app/api/settlementList/query';
+
+const labels = ['이름', '요청일', '계정', '요청금액', '남은금액'];
 
 const SettlementListClient = () => {
   const {data} = settlementListQuery();
@@ -11,6 +15,8 @@ const SettlementListClient = () => {
   return (
     <div className='w-full'>
       <Title>정산관리</Title>
+      <ListBoardHeader labels={labels} isCheckBox={true} />
+      <SettlementListBoardContents isCheckBox={true} contents={data} />
     </div>
   );
 };
--- apps/admin/app/@content/settlement/_components/SettlementListBoardContents/index.tsx
+++ apps/admin/app/@content/settlement/_components/SettlementListBoardContents/index.tsx
@@ -1,13 +1,16 @@
-import ListBoardContent from '../../../../_components/ListBoardContent';
-import UserList from '@/app/api/userList/type';
+import ListBoardContent from '@/app/_components/ListBoardContent';
+import SettlementList from '@/app/api/settlementList/type';
 import formatDate from '@/app/utils/formatDate';
 
 type Props = {
   isCheckBox: boolean;
-  contents: UserList;
+  contents: SettlementList;
 };
 
-const UserListBoardContents: React.FC<Props> = ({isCheckBox, contents}) => {
+const SettlementListBoardContents: React.FC<Props> = ({
+  isCheckBox,
+  contents,
+}) => {
   return (
     <div className='mx-[23px] mb-[32px]'>
       {contents.data.users.map(content => (
@@ -18,9 +21,10 @@ const UserListBoardContents: React.FC<Props> = ({isCheckBox, contents}) => {
                 <input type='checkbox' className='mx-[30px] my-[9px]' />
               )}
               <ListBoardContent content={content.name} />
-              <ListBoardContent content={formatDate(content.createdAt)} />
+              <ListBoardContent content={formatDate(content.requestDate)} />
               <ListBoardContent content={content.email} />
-              <ListBoardContent content={content.wraffleCount} />
+              <ListBoardContent content={content.requestCharge} />
+              <ListBoardContent content={content.remainCharge} />
             </div>
             <div className='flex w-[148px] items-center justify-center'>
               <button className='rounded bg-[#F2F4F6] px-[17.5px] py-[8px] text-[#4E5968]'>
@@ -35,4 +39,4 @@ const UserListBoardContents: React.FC<Props> = ({isCheckBox, contents}) => {
   );
 };
 
-export default UserListBoardContents;
+export default SettlementListBoardContents;
